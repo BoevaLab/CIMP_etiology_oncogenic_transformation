7823a699b0bae52a1107563b7b89d9b35023e8d1
josephineyates <josephine.yates@polytechnique.edu>
2021-10-15 11:48:03 +0200

    added data and corrected minor mistakes

--- DESeq_GEX.R
+++ DESeq_GEX.R
@@ -1,7 +1,7 @@
 library(DESeq2)
 library(data.table)
 
-directory <- "C:\\Users\\josep\\Documents\\ETHZ\\Boeva_lab\\CIMP_unsupervised_cancer\\code\\gene_counts"
+directory <- "path\to\raw\genes\counts"
 all_files <- list.files(path=directory)
 #### FOR ALL TCGA CANCER TYPES
 
@@ -10,7 +10,7 @@ for(file in all_files){
   cancer <- strsplit(strsplit(file,split="-")[[1]][2],split=".htseq")[[1]][1]
   cts <- as.data.frame(fread(paste(directory,file,sep="\\")))
 
-  file_clust <- paste0("C:\\Users\\josep\\Documents\\ETHZ\\Boeva_lab\\CIMP_unsupervised_cancer\\code\\cluster_memb\\",cancer,"_cluster_memb_new.csv")
+  file_clust <- paste0("path\to\CIMPcluster\membership",cancer,"_cluster_memb_new.csv")
   coldata <- as.data.frame(fread(file_clust))
   if(cancer=="LAML"){
     colnames(cts) <- sub("03A", "03", colnames(cts))
@@ -61,7 +61,7 @@ for(file in all_files){
   res$pvalue[is.na(res$pvalue)] <- 1
 
 
-  write.csv(as.data.frame(res[which(res$pvalue<0.05),c("log2FoldChange","pvalue")]),file=paste0("C:\\Users\\josep\\Downloads\\",cancer,"_diff_gene_expression.csv"))
+  write.csv(as.data.frame(res[which(res$pvalue<0.05),c("log2FoldChange","pvalue")]),file=paste0("path\to\save\",cancer,"_diff_gene_expression.csv"))
 
 }
 
